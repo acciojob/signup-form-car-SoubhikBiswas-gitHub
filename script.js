3ba266ce505477339913382b05109d496304b312
SOUBHIK BISWAS <54506603+SoubhikBiswas-gitHub@users.noreply.github.com>
2024-04-22 15:42:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-//your code here
-bbcbng
+// take the input from the user
+const number = prompt('Enter the number: ');
+
+const result = Math.sqrt(number);
+console.log(`The square root of ${number} is ${result}`);
